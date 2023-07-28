d11e81e83519c4511e0d04b85a806492b6d9b032
Viacheslav Shylko <116965660+Laslold@users.noreply.github.com>
2023-07-28 10:59:59 +0300

    delet persist

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-// import store from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from 'redux/store';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
-  </Provider>
 
-  // </React.StrictMode>
+import { store } from 'redux/store';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
